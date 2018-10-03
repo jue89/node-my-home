263e0f4782e06178657091adc0d92f1a71565124
Jue <me@jue.yt>
2018-10-03 17:53:22 +0200

    curiosity: Added sensors

--- curiosity/log.js
+++ curiosity/log.js
@@ -1,18 +1,6 @@
-const tsfoo = require('tsfoo');
-
-module.exports = async (ftrm) => {
-	// Open database
-	const db = await tsfoo.openDB('/media/data/my-home');
-
-	// Create multiplexer that decides to write to wich series based on the pipe
-	const multiplexer = tsfoo.createMultiplexer(db);
-	multiplexer.on('internalError', console.error);
-
-	// Return config for our instance
-	return [require('ftrm-basic/to-writable'), {
-		input: '#',
-		stream: multiplexer,
-		format: (value, timestamp, src) => ({series: src.event, value, timestamp})
-	}];
-};
-
+module.exports = [require('ftrm-basic/to-writable'), {
+	input: '#',
+	stream: process.stdout,
+	dontCloseStream: true,
+	format: (value, ts, src) => `${new Date(ts)}\t${src.event}\t${value.toString()}\n`
+}];
