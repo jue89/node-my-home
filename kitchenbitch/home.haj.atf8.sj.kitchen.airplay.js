703464eb08fa4e3dbb595e1e8b28b94c38d07373
Jue <me@jue.yt>
2018-11-30 23:22:42 +0100

    kitchenbitch: Turn on speakers if airplay is playing music

--- kitchenbitch/home.haj.atf8.sj.kitchen.airplay.js
+++ kitchenbitch/home.haj.atf8.sj.kitchen.airplay.js
@@ -10,5 +10,12 @@ module.exports = [
 		output: [{pipe: 'home.haj.atf8.sj.kitchen.airplay.playing', throttle: 10 * 60 * 1000}],
 		inject: () => readFile('/proc/asound/card0/pcm0p/sub0/status').then((d) => d != 'closed'),
 		interval: 10000
-	}]
+	}],
+
+	// Speaker
+	[require('ftrm-gpio/out'), {
+		input: 'home.haj.atf8.sj.kitchen.airplay.playing',
+		gpio: 5,
+		default: false
+	}],
 ];
