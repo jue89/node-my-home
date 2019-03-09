82d855fead809164f280ba7fcb1ce4f77679b115
Jue <me@jue.yt>
2019-03-09 18:45:41 +0100

    curiosity: Changed logging format

--- curiosity/log.js
+++ curiosity/log.js
@@ -2,5 +2,12 @@ module.exports = [require('ftrm-basic/to-writable'), {
 	input: '#',
 	stream: process.stdout,
 	dontCloseStream: true,
-	format: (value, ts, src) => `${new Date(ts)}\t${src.event}\t${value.toString()}\n`
+	format: (value, ts, src) => {
+		let padding = 80 - src.event.length;
+		if (typeof value == 'number') value = value.toFixed(2);
+		else value = value.toString();
+		padding -= value.length;
+		if (padding < 1) padding = 1;
+		return `${src.event}${' '.repeat(padding)}${value.toString()}\n`;
+	}
 }];
