d00b3ab80ceb0a8cef63750401ef2ba3d015bdcb
Jue <me@jue.yt>
2019-03-10 16:10:55 +0100

    kitchenbitch: Added heating control

---
+++ kitchenbitch/home.haj.atf8.sj.heating.js
@@ -0,0 +1,57 @@
+const url = require('url');
+const http = require('http');
+const secrets = require('./secrets.json');
+
+function delay (msec) {
+	return new Promise((resolve) => setTimeout(resolve, msec));
+}
+
+function switchRelay (on) {
+	const state = on ? 'on' : 'off';
+	const query = url.parse(`http://${secrets.therminator.credentials}@shelly-therminator.lan.13pm.eu/relay/0?turn=${state}`);
+	return new Promise((resolve, reject) => http.get(query, (res) => {
+		const chunks = [];
+		res.on('data', (chunk) => chunks.push(chunk));
+		res.on('end', () => {
+			const body = Buffer.concat(chunks).toString();
+			if (res.statusCode === 200) {
+				const response = JSON.parse(body);
+				resolve(response.ison);
+			} else {
+				reject(new Error(body));
+			}
+		});
+	}).on('error', (err) => reject(err)));
+}
+
+module.exports = [
+	// Find the highest temperature request
+	[require('ftrm-basic/select'), {
+		input: [
+			{pipe: 'home.haj.atf8.sj.jue.radiator.desiredTemperature_degC', expire: 10 * 60 * 1000},
+			{pipe: 'home.haj.atf8.sj.steffen.radiator.desiredTemperature_degC', expire: 10 * 60 * 1000},
+			{pipe: 'home.haj.atf8.sj.kitchen.radiator.desiredTemperature_degC', expire: 10 * 60 * 1000},
+			{pipe: 'home.haj.atf8.sj.leo.radiator.desiredTemperature_degC', expire: 10 * 60 * 1000}
+		],
+		output: [{pipe: 'home.haj.atf8.sj.heating.desiredSupplyTemperature_degC', throttle: 5 * 60 * 1000}],
+		weight: 'max'
+	}],
+
+	// Enable the pump if energy is requested
+	[require('ftrm-basic/map'), {
+		input: 'home.haj.atf8.sj.heating.desiredSupplyTemperature_degC',
+		output: [{pipe: 'home.haj.atf8.sj.heating.pump.desiredOnState', throttle: 5 * 60 * 1000}],
+		map: (temp) => temp > 25
+	}],
+	[require('ftrm-basic/map'), {
+		input: 'home.haj.atf8.sj.heating.pump.desiredOnState',
+		output: 'home.haj.atf8.sj.heating.pump.actualOnState',
+		map: async (on) => {
+			// Since it takes up to 2 minutes for the valves to drive, we insert a delay
+			await delay(2 * 60 * 1000);
+			const ison = await switchRelay(on);
+			return ison;
+		}
+	}]
+];
+
