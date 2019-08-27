3a31c18ee5e12af895b871658dbea538ac9bd214
Jue <me@jue.yt>
2019-08-27 23:19:35 +0200

    entropy: Added PC switch

---
+++ entropy/home.haj.atf8.sj.jue.pc.js
@@ -0,0 +1,123 @@
+const http = require('http');
+const url = require('url');
+const secrets = require('./secrets.json');
+
+function query (host, cmd) {
+	const query = url.parse(`http://${host}/cm?user=${secrets.shelly.user}&password=${secrets.shelly.password}&cmnd=${cmd}`);
+	return new Promise((resolve, reject) => http.get(query, (res) => {
+		const chunks = [];
+		res.on('data', (chunk) => chunks.push(chunk));
+		res.on('end', () => {
+			const body = Buffer.concat(chunks).toString();
+			if (res.statusCode === 200) {
+				const response = JSON.parse(body);
+				resolve(response);
+			} else {
+				reject(new Error(body));
+			}
+		});
+	}).on('error', (err) => reject(err)));
+}
+
+async function fetchPower (host) {
+	const body = await query(host, 'Status%2010');
+	return body.StatusSNS.ENERGY;
+}
+
+async function switchRelay (host, on) {
+	const body = await query(host, on ? 'Power On' : 'Power Off');
+	return body.POWER === 'ON';
+}
+
+module.exports = [
+	// Master: Power measurement
+	[require('ftrm-basic/inject-many'), {
+		output: {
+			'Power': 'home.haj.atf8.sj.jue.pc.master.activePower',
+			'ApparentPower': 'home.haj.atf8.sj.jue.pc.master.apparentPower',
+			'ReactivePower': 'home.haj.atf8.sj.jue.pc.master.reactivePower',
+		},
+		inject: () => fetchPower('shelly-bart.lan.13pm.eu'),
+		interval: 20 * 1000
+	}],
+
+	// Master: Power switch
+	[require('ftrm-homekit')('Switch'), {
+		input: { 'On': 'home.haj.atf8.sj.jue.pc.master.actualOnState' },
+		output: { 'On': 'home.haj.atf8.sj.jue.pc.master.desiredOnState.switch' },
+		displayName: 'PC'
+	}],
+
+	// Master: Power based switch: Keep the relay on as long the PC is powered on
+	[require('ftrm-basic/map'), {
+		input: 'home.haj.atf8.sj.jue.pc.master.activePower',
+		output: 'home.haj.atf8.sj.jue.pc.master.desiredOnState.power',
+		// true -> Keep power on; undefined -> Ask someone else ...
+		map: (pwr) => (pwr > 15) ? true : undefined
+	}],
+
+	// Master: Override switch: Keeps the PC powered on - no matter whats going on
+	[require('ftrm-homekit')('Switch'), {
+		output: { 'On': 'home.haj.atf8.sj.jue.pc.master.desiredOnState.override' },
+		displayName: 'PC Override'
+	}],
+
+	// Master: Select the power on state based
+	[require('ftrm-basic/select'), {
+		input: [
+			{pipe: 'home.haj.atf8.sj.jue.pc.master.desiredOnState.switch', expire: 50 * 1000},
+			{pipe: 'home.haj.atf8.sj.jue.pc.master.desiredOnState.power', expire: 30 * 1000},
+			{pipe: 'home.haj.atf8.sj.jue.pc.master.desiredOnState.override'},
+			{value: false}
+		],
+		output: [
+			{pipe: 'home.haj.atf8.sj.jue.pc.master.desiredOnState', throttle: 10 * 60 * 1000}
+		],
+		weight: 'prio'
+	}],
+
+	// Master: Switch the relay ...
+	[require('ftrm-basic/map'), {
+		input: 'home.haj.atf8.sj.jue.pc.master.desiredOnState',
+		output: 'home.haj.atf8.sj.jue.pc.master.actualOnState',
+		map: (on) => switchRelay('shelly-bart.lan.13pm.eu', on)
+	}],
+
+	// Slave: Power measurement
+	[require('ftrm-basic/inject-many'), {
+		output: {
+			'Power': 'home.haj.atf8.sj.jue.pc.slave.activePower',
+			'ApparentPower': 'home.haj.atf8.sj.jue.pc.slave.apparentPower',
+			'ReactivePower': 'home.haj.atf8.sj.jue.pc.slave.reactivePower',
+		},
+		inject: () => fetchPower('shelly-bart-periph.lan.13pm.eu'),
+		interval: 20 * 1000
+	}],
+
+	// Slave: Override switch: Keeps the PC powered on - no matter whats going on
+	[require('ftrm-homekit')('Switch'), {
+		output: { 'On': 'home.haj.atf8.sj.jue.pc.slave.desiredOnState.override' },
+		displayName: 'PC Periph Override'
+	}],
+
+	// Slave: Select the power on state based
+	[require('ftrm-basic/combine'), {
+		input: [
+			{pipe: 'home.haj.atf8.sj.jue.pc.master.actualOnState'},
+			{pipe: 'user.jue.present.atf8'},
+			{pipe: 'home.haj.atf8.sj.jue.pc.slave.desiredOnState.override'}
+		],
+		output: [
+			{pipe: 'home.haj.atf8.sj.jue.pc.slave.desiredOnState', throttle: 10 * 60 * 1000}
+		],
+		combineExpiredInputs: true,
+		combine: (masterOnState, juePresent, override) => override || (masterOnState && juePresent) || false
+	}],
+
+	// Slave: Switch the relay ...
+	[require('ftrm-basic/map'), {
+		input: 'home.haj.atf8.sj.jue.pc.slave.desiredOnState',
+		output: 'home.haj.atf8.sj.jue.pc.slave.actualOnState',
+		map: (on) => switchRelay('shelly-bart-periph.lan.13pm.eu', on)
+	}],
+];
