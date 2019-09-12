56909d7fed90487b56b0b24c1e12d4a10d0170a7
Jue <me@jue.yt>
2019-09-12 11:35:07 +0200

    curiosity: Added inspector

--- curiosity/log.js
+++ curiosity/log.js
@@ -1,16 +1,22 @@
 const Journal = require('systemd-journald');
 const log = new Journal({syslog_identifier: 'my-home'});
 
-module.exports = [require('ftrm-basic/generic'), {
-	name: 'log-to-journal',
-	input: [{pipe: '#', spy: true}],
-	factory: (i) => {
-		const log = new Journal({syslog_identifier: 'my-home'});
-		i[0].on('update', (value, timestamp, src) => {
-			if (typeof value == 'number') value = value.toFixed(2);
-			const pipe = src.event;
-			value = `${value}`;
-			log.debug(`${pipe.padEnd(80)} ${value.padStart(20)}`, {pipe});
-		});
-	}
-}];
+module.exports = [
+	[require('ftrm-basic/generic'), {
+		name: 'log-to-journal',
+		input: [{pipe: '#', spy: true}],
+		factory: (i) => {
+			const log = new Journal({syslog_identifier: 'my-home'});
+			i[0].on('update', (value, timestamp, src) => {
+				if (typeof value == 'number') value = value.toFixed(2);
+				const pipe = src.event;
+				value = `${value}`;
+				log.debug(`${pipe.padEnd(80)} ${value.padStart(20)}`, {pipe});
+			});
+		}
+	}],
+	[require('ftrm-inspector'), {
+		name: 'inspector',
+		bind: {host: '::', port: 3876}
+	}]
+];
