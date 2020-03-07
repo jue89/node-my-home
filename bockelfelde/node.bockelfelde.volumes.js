f11fe83cd6d84cb424464c80b4b173bc9abe734e
Jue <me@jue.yt>
2020-03-07 20:45:54 +0100

    Added bockelfelde

---
+++ bockelfelde/node.bockelfelde.volumes.js
@@ -0,0 +1,7 @@
+const getMdstat = require('../_lib/mdstat.js');
+module.exports = [require('ftrm-basic/inject'), {
+	name: 'mdstat-md0',
+	output: 'node.bockelfelde.volumes.md0.online',
+	inject: () => getMdstat('md0'),
+	interval: 20 * 60 * 1000
+}];
