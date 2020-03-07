783b987aba0e643aac135ca637d8808344d695fd
Jue <me@jue.yt>
2020-03-07 20:06:46 +0100

    Added manual node bootstraping

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const fs = require('fs');
 const path = require('path');
 const FTRM = require('ftrm');
 
+const port = 4816;
+
 // Dry run
 const dryRun = process.argv[2] === '--check';
 
 // Helper for loading the PKI stuff
 const getPem = (file) => fs.readFileSync(path.join(__dirname, '_pki', file));
+
+// Node discovery
+const discovery = (process.env.BOOTSTRAP) ? {
+	host: process.env.BOOTSTRAP,
+	port
+} : require('tubemail-mdns');
 
 // By default use the hostname as node name
 const node = process.env.NODE || os.hostname();
@@ -20,5 +28,7 @@ FTRM({
 	autoRunDir: path.join(__dirname, node),
 	log: `${logScope}-${logSink}`,
 	node,
+	discovery,
+	port,
 	dryRun
 }).catch(console.error);
