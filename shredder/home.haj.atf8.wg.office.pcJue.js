a759d05d8da92fba936b74e28a3191d582f97775
Juergen Fitschen <me@jue.yt>
2020-06-07 16:05:39 +0200

    shredder: moved PC shelly logic to common shelly driver

--- shredder/home.haj.atf8.wg.office.pcJue.js
+++ shredder/home.haj.atf8.wg.office.pcJue.js
@@ -1,60 +1,38 @@
-const http = require('http');
-const url = require('url');
 const secrets = require('./secrets.json');
-
-function query (host, cmd) {
-	const query = url.parse(`http://${host}/cm?user=${secrets.shelly.user}&password=${secrets.shelly.password}&cmnd=${cmd}`);
-	return new Promise((resolve, reject) => http.get(query, (res) => {
-		const chunks = [];
-		res.on('data', (chunk) => chunks.push(chunk));
-		res.on('end', () => {
-			const body = Buffer.concat(chunks).toString();
-			if (res.statusCode === 200) {
-				const response = JSON.parse(body);
-				resolve(response);
-			} else {
-				reject(new Error(body));
-			}
-		});
-	}).on('error', (err) => reject(err)));
-}
-
-async function fetchPower (host) {
-	const body = await query(host, 'Status%2010');
-	return body.StatusSNS.ENERGY;
-}
-
-async function switchRelay (host, on) {
-	const body = await query(host, on ? 'Power On' : 'Power Off');
-	return body.POWER === 'ON';
-}
+const BASE = __filename.slice(__dirname.length + 1, -3);
 
 module.exports = [
-	// Master: Power measurement
-	[require('ftrm-basic/inject-many'), {
-		name: 'pc-power',
-		output: {
-			'Power': 'home.haj.atf8.wg.office.pcJue.master.activePower',
-			'ApparentPower': 'home.haj.atf8.wg.office.pcJue.master.apparentPower',
-			'ReactivePower': 'home.haj.atf8.wg.office.pcJue.master.reactivePower',
+	// Master: Relay
+	[require('../_lib/shellyPlug.js'), {
+		name: 'pc-relay',
+		input: {
+			'Relay': `${BASE}.master.desiredOnState`
 		},
-		inject: () => fetchPower('100.64.0.2'),
-		interval: 20 * 1000
+		output: {
+			'Relay': `${BASE}.master.actualOnState`,
+			'Power': `${BASE}.master.power_W`,
+			'ApparentPower': `${BASE}.master.apparentPower_VA`,
+			'ReactivePower': `${BASE}.master.reactivePower_var`
+		},
+		host: '100.64.0.2',
+		user: secrets.shelly.user,
+		password: secrets.shelly.password,
+		powerReadoutInterval: 20 * 1000
 	}],
 
 	// Master: Power switch
 	[require('ftrm-homekit')('Switch'), {
 		name: 'pc-switch-homekit',
-		input: { 'On': 'home.haj.atf8.wg.office.pcJue.master.actualOnState' },
-		output: { 'On': 'home.haj.atf8.wg.office.pcJue.master.desiredOnState.switch' },
+		input: { 'On': `${BASE}.master.actualOnState` },
+		output: { 'On': `${BASE}.master.desiredOnState.switch` },
 		displayName: 'PC'
 	}],
 
 	// Master: Power based switch: Keep the relay on as long the PC is powered on
 	[require('ftrm-basic/map'), {
 		name: 'pc-switch-power',
-		input: 'home.haj.atf8.wg.office.pcJue.master.activePower',
-		output: 'home.haj.atf8.wg.office.pcJue.master.desiredOnState.power',
+		input: `${BASE}.master.activePower`,
+		output: `${BASE}.master.desiredOnState.power`,
 		// true -> Keep power on; undefined -> Ask someone else ...
 		map: (pwr) => (pwr > 15) ? true : undefined
 	}],
@@ -62,7 +40,7 @@ module.exports = [
 	// Master: Override switch: Keeps the PC powered on - no matter whats going on
 	[require('ftrm-homekit')('Switch'), {
 		name: 'pc-switch-override',
-		output: { 'On': 'home.haj.atf8.wg.office.pcJue.master.desiredOnState.override' },
+		output: { 'On': `${BASE}.master.desiredOnState.override` },
 		displayName: 'PC Override'
 	}],
 
@@ -70,41 +48,39 @@ module.exports = [
 	[require('ftrm-basic/select'), {
 		name: 'pc-switch',
 		input: [
-			{pipe: 'home.haj.atf8.wg.office.pcJue.master.desiredOnState.switch', expire: 50 * 1000, logLevelExpiration: null},
-			{pipe: 'home.haj.atf8.wg.office.pcJue.master.desiredOnState.power', expire: 90 * 1000},
-			{pipe: 'home.haj.atf8.wg.office.pcJue.master.desiredOnState.override'},
+			{pipe: `${BASE}.master.desiredOnState.switch`, expire: 50 * 1000, logLevelExpiration: null},
+			{pipe: `${BASE}.master.desiredOnState.power`, expire: 90 * 1000},
+			{pipe: `${BASE}.master.desiredOnState.override`},
 			{value: false}
 		],
 		output: [
-			{pipe: 'home.haj.atf8.wg.office.pcJue.master.desiredOnState', throttle: 10 * 60 * 1000}
+			{pipe: `${BASE}.master.desiredOnState`, throttle: 10 * 60 * 1000}
 		],
 		weight: 'prio'
 	}],
 
-	// Master: Switch the relay ...
-	[require('ftrm-basic/map'), {
+	// Slave: Relay
+	[require('../_lib/shellyPlug.js'), {
 		name: 'pc-relay',
-		input: 'home.haj.atf8.wg.office.pcJue.master.desiredOnState',
-		output: 'home.haj.atf8.wg.office.pcJue.master.actualOnState',
-		map: (on) => switchRelay('100.64.0.2', on)
-	}],
-
-	// Slave: Power measurement
-	[require('ftrm-basic/inject-many'), {
-		name: 'periph-power',
-		output: {
-			'Power': 'home.haj.atf8.wg.office.pcJue.slave.activePower',
-			'ApparentPower': 'home.haj.atf8.wg.office.pcJue.slave.apparentPower',
-			'ReactivePower': 'home.haj.atf8.wg.office.pcJue.slave.reactivePower',
+		input: {
+			'Relay': `${BASE}.slave.desiredOnState`
 		},
-		inject: () => fetchPower('100.64.0.3'),
-		interval: 20 * 1000
+		output: {
+			'Relay': `${BASE}.slave.actualOnState`,
+			'Power': `${BASE}.slave.power_W`,
+			'ApparentPower': `${BASE}.slave.apparentPower_VA`,
+			'ReactivePower': `${BASE}.slave.reactivePower_var`
+		},
+		host: '100.64.0.3',
+		user: secrets.shelly.user,
+		password: secrets.shelly.password,
+		powerReadoutInterval: 20 * 1000
 	}],
 
 	// Slave: Override switch: Keeps the PC powered on - no matter whats going on
 	[require('ftrm-homekit')('Switch'), {
 		name: 'periph-switch-override',
-		output: { 'On': 'home.haj.atf8.wg.office.pcJue.slave.desiredOnState.override' },
+		output: { 'On': `${BASE}.slave.desiredOnState.override` },
 		displayName: 'PC Periph Override'
 	}],
 
@@ -112,23 +88,15 @@ module.exports = [
 	[require('ftrm-basic/combine'), {
 		name: 'periph-switch',
 		input: [
-			{pipe: 'home.haj.atf8.wg.office.pcJue.master.actualOnState'},
-			{pipe: 'user.jue.present.atf8'},
-			{pipe: 'user.fpi.present.atf8'},
-			{pipe: 'home.haj.atf8.wg.office.pcJue.slave.desiredOnState.override'}
+			{pipe: `${BASE}.master.actualOnState`},
+			{pipe: `user.jue.present.atf8`},
+			{pipe: `user.fpi.present.atf8`},
+			{pipe: `${BASE}.slave.desiredOnState.override`}
 		],
 		output: [
-			{pipe: 'home.haj.atf8.wg.office.pcJue.slave.desiredOnState', throttle: 10 * 60 * 1000}
+			{pipe: `${BASE}.slave.desiredOnState`, throttle: 10 * 60 * 1000}
 		],
 		combineExpiredInputs: true,
 		combine: (masterOnState, juePresent, fpiPresent, override) => override || (masterOnState && (juePresent || fpiPresent)) || false
-	}],
-
-	// Slave: Switch the relay ...
-	[require('ftrm-basic/map'), {
-		name: 'periph-relay',
-		input: 'home.haj.atf8.wg.office.pcJue.slave.desiredOnState',
-		output: 'home.haj.atf8.wg.office.pcJue.slave.actualOnState',
-		map: (on) => switchRelay('100.64.0.3', on)
 	}],
 ];
