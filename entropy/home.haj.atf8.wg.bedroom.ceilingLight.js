36a27f8daa193f651a5b3b40f0bdd93bb187cbd6
Jue <me@jue.yt>
2020-10-22 18:53:50 +0200

    entropy: add ceiling light shelly

---
+++ entropy/home.haj.atf8.wg.bedroom.ceilingLight.js
@@ -0,0 +1,27 @@
+const BASE = __filename.slice(__dirname.length + 1, -3);
+const secrets = require('./secrets.json');
+
+module.exports = [
+	// Homekit Switch
+	[require('ftrm-homekit')('Switch'), {
+		name: 'ceilingLight-homekit',
+		input: { 'On': `${BASE}.actualOnState` },
+		output: { 'On': `${BASE}.desiredOnState` },
+		displayName: 'Ceiling Light'
+	}],
+
+	// Shelly Relay
+	[require('../_lib/shellyPlug.js'), {
+		name: 'ceilingLight-shelly',
+		input: {
+			'Relay': `${BASE}.desiredOnState`
+		},
+		output: {
+			'Relay': `${BASE}.actualOnState`
+		},
+		host: '100.64.0.5',
+		user: secrets.shelly.user,
+		password: secrets.shelly.password,
+		readbackInterval: 5 * 1000
+	}]
+];
