55557b8523f51d23d341ac741933eaf5973f62b8
Juergen Fitschen <me@jue.yt>
2020-11-27 00:49:52 +0100

    shredder: MOAR LEDs!

--- shredder/home.haj.atf8.wg.officeJue.rack.js
+++ shredder/home.haj.atf8.wg.officeJue.rack.js
@@ -2,13 +2,14 @@ const spi = require('spi-device');
 
 const BASE = __filename.slice(__dirname.length + 1, -3);
 
-const COLOR_OK = [8, 0, 32];
+const COLOR_OK = [64, 0, 128];
 const COLOR_FAIL = [255, 0, 32];
 
 const SPI_BUS = 0;
 const SPI_CS = 0;
 const SPI_CLK = 100000;
-const LED_COUNT = 17 * 2;
+const LED_COUNT_SIDE = 17;
+const LED_COUNT_TOP = 15;
 
 module.exports = [
 	[require('ftrm-basic/combine'), {
@@ -35,10 +36,17 @@ module.exports = [
 
 			inputs[0].on('update', ([r, g, b]) => {
 				const bufClear = Buffer.from([0x00, 0x00, 0x00, 0x00]);
-				const bufColor = Buffer.from([0xff, b, g, r]);
+				const bufColorSide = Buffer.from([0xe7, b, g, r]);
+				const bufColorTop = Buffer.from([0xff, b, g, r]);
 				const bufs = [bufClear];
-				for (let i = 0; i < LED_COUNT; i++) {
-					bufs.push(bufColor);
+				for (let i = 0; i < LED_COUNT_SIDE; i++) {
+					bufs.push(bufColorSide);
+				}
+				for (let i = 0; i < LED_COUNT_TOP; i++) {
+					bufs.push(bufColorTop);
+				}
+				for (let i = 0; i < LED_COUNT_SIDE; i++) {
+					bufs.push(bufColorSide);
 				}
 				bufs.push(bufClear);
 				const sendBuffer = Buffer.concat(bufs);
