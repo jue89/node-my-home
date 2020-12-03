9a9b5e7f747c696e0e5e3211e80b5ea88a4a213b
Juergen Fitschen <me@jue.yt>
2020-12-03 20:55:16 +0100

    hqcam: moved from RGB to HSV colors

--- _lib/sk9822.js
+++ _lib/sk9822.js
@@ -6,20 +6,36 @@ const STEP_DURATION = 1000 / 25;
 
 const delay = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
 
-module.exports = ({spiFactory, input, ledCount}) => [require('ftrm-basic/generic'), {
+const hsv2rgb = ([h, s, v]) => {
+	const c = v * s;
+	const x = c * (1 - Math.abs((h / 60) % 2 - 1));
+	const m = v - c;
+	const [r0, g0, b0] = (h < 60) ? [c, x, 0]
+		: (h < 120) ? [x, c, 0]
+		: (h < 180) ? [0, c, x]
+		: (h < 240) ? [0, x, c]
+		: (h < 300) ? [x, 0, c]
+		: [x, 0, c];
+	return [(r0 + m) * 255, (g0 + m) * 255, (b0 + m) * 255];
+};
+
+const bufClear = Buffer.from([0x00, 0x00, 0x00, 0x00]);
+
+module.exports = ({spiFactory, input, ledCount, filter}) => [require('ftrm-basic/generic'), {
 	name: 'leds',
 	input,
 	factory: (inputs) => {
-		const leds = spiFactory()
-		const display = ([r, g, b]) => {
-			const bufClear = Buffer.from([0x00, 0x00, 0x00, 0x00]);
-			const bufColor = Buffer.from([0xff, Math.round(b), Math.round(g), Math.round(r)]);
-			const bufs = [bufClear];
-			for (let i = 0; i < ledCount; i++) {
-				bufs.push(bufColor);
-			}
-			bufs.push(bufClear);
-			const sendBuffer = Buffer.concat(bufs);
+		// Pass-thru filter if no filter has been specified
+		if (!filter) filter = (x) => x;
+
+		const leds = spiFactory();
+
+		// Displays a list of RGB colors
+		const display = (colors) => {
+			const bufferList = colors.map(([r, g, b]) => Buffer.from([0xff, Math.round(b), Math.round(g), Math.round(r)]));
+			bufferList.unshift(bufClear);
+			bufferList.push(bufClear);
+			const sendBuffer = Buffer.concat(bufferList);
 			leds.transfer([{
 				sendBuffer,
 				byteLength: sendBuffer.length,
@@ -27,26 +43,36 @@ module.exports = ({spiFactory, input, ledCount}) => [require('ftrm-basic/generic
 			}], () => {});
 		}
 
-		let currentColor = [0, 0, 0];
+		// Fill current color array
+		let currentColors = [];
+		for (let i = 0; i < ledCount; i++) {
+			currentColors.push([0, 0, 0]);
+		}
+
+		// Fades to a HSV color
 		const sem = qsem(1);
 		const fade = (nextColor) => sem.limit(async () => {
-			const [fromR, fromG, fromB] = currentColor;
-			const [toR, toG, toB] = nextColor;
-			const stepR = (toR - fromR) / STEPS;
-			const stepG = (toG - fromG) / STEPS;
-			const stepB = (toB - fromB) / STEPS;
+			const targetColors = [];
+			for (let i = 0; i < ledCount; i++) {
+				targetColors.push(hsv2rgb(filter(nextColor, i)));
+			}
 
-			let r = fromR;
-			let g = fromG;
-			let b = fromB;
+			const steps = targetColors.map(([toR, toG, toB], n) => {
+				const [fromR, fromG, fromB] = currentColors[n];
+				const stepR = (toR - fromR) / STEPS;
+				const stepG = (toG - fromG) / STEPS;
+				const stepB = (toB - fromB) / STEPS;
+				return [stepR, stepG, stepB];
+			});
+
 			for (let i = 0; i < STEPS; i++) {
-				r += stepR;
-				g += stepG;
-				b += stepB;
-				display([r, g, b]);
+				currentColors = currentColors.map(([r, g, b], n) => {
+					const [stepR, stepG, stepB] = steps[n];
+					return [r + stepR, g + stepG, b + stepB];
+				});
+				display(currentColors);
 				await delay(STEP_DURATION);
 			}
-			currentColor = nextColor;
 		});
 
 		inputs[0].on('update', (nextColor) => fade(nextColor));
@@ -54,4 +80,3 @@ module.exports = ({spiFactory, input, ledCount}) => [require('ftrm-basic/generic
 		return () => leds.closeSync();
 	}
 }];
-
--- hqcam/home.haj.wg.officeJue.desk.js
+++ hqcam/home.haj.wg.officeJue.desk.js
@@ -4,7 +4,7 @@ const BASE = __filename.slice(__dirname.length + 1, -3);
 
 module.exports = [
 	require('../_lib/sk9822.js')({
-		input: `${BASE}.color_rgb`,
+		input: `${BASE}.color_hsv`,
 		spiFactory: () => spi.openSync(0, 0),
 		ledCount: 36
 	}),
@@ -12,7 +12,7 @@ module.exports = [
 	[require('ftrm-basic/map'), {
 		name: 'color',
 		input: `home.haj.atf8.wg.officeJue.pcJue.inUse`,
-		output: `${BASE}.color_rgb`,
-		map: (inUse) => inUse ? [24, 0, 64] : [0, 0, 0]
-	}]
+		output: `${BASE}.color_hsv`,
+		map: (inUse) => inUse ? [270, 1, 0.5] : [0, 0, 0]
+	}],
 ]
