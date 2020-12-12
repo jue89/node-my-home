4a2b476a14594a7c948ea36f6b18a360b24978b6
Juergen Fitschen <me@jue.yt>
2020-12-12 20:46:46 +0100

    lib/shellyPlug: power readback after switching

--- _lib/shellyPlug.js
+++ _lib/shellyPlug.js
@@ -3,6 +3,10 @@ const url = require('url');
 const qsem = require('qsem');
 const StatefulError = require('ftrm/stateful-error');
 
+const POWER_ENDPOINTS = ['Power', 'ApparentPower', 'ReactivePower'];
+
+const delay = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
+
 function shellyFacotry ({host, user, password}) {
 	let baseurl = `http://${host}/cm?`;
 	if (user) baseurl += `user=${user}&`;
@@ -55,33 +59,42 @@ function factory (opts, input, output, log) {
 	});
 
 	// Relay state readback
-	if (opts.readbackInterval && output.Relay) {
-		const publishRelayState = () => safeQueryShelly('State').then((body) => {
-			if (!body) return;
-			output.Relay.value = body.POWER === 'ON';
-		});
+	async function publishRelayState () {
+		if (!output.Relay) return;
+		const body = await safeQueryShelly('State');
+		if (!body) return;
+		output.Relay.value = body.POWER === 'ON';
+	}
+	if (opts.readbackInterval) {
 		const interval = setInterval(publishRelayState, opts.readbackInterval);
 		exitCallbacks.push(() => clearInterval(interval));
 	}
 
 	// Relay power readback
-	if (opts.powerReadoutInterval) {
-		const publishRelayPower = () => safeQueryShelly('Status%2010').then((body) => {
-			if (!body) return;
-			Object.entries(body.StatusSNS.ENERGY).forEach(([key, value]) => {
-				if (output[key]) output[key].value = value;
-			})
+	const powerOutputs = POWER_ENDPOINTS.filter((key) => output[key]);
+	async function publishRelayPower () {
+		if (powerOutputs.length === 0) return;
+		const body = await safeQueryShelly('Status%2010');
+		if (!body) return;
+		powerOutputs.forEach((key) => {
+			output[key].value = body.StatusSNS.ENERGY[key];
 		});
+	}
+	if (opts.powerReadoutInterval) {
 		const interval = setInterval(publishRelayPower, opts.powerReadoutInterval);
 		exitCallbacks.push(() => clearInterval(interval));
 	}
 
 	// Forward switching commands
+	async function setRelay (on) {
+		const body = await safeQueryShelly(on ? 'Power On' : 'Power Off');
+		if (!body) return;
+		await delay(300);
+		await publishRelayState();
+		await delay(700);
+		await publishRelayPower();
+	}
 	if (input.Relay) {
-		const setRelay = (on) => safeQueryShelly(on ? 'Power On' : 'Power Off').then((body) => {
-			if (!body) return;
-			if (output.Relay) output.Relay.value = body.POWER === 'ON';
-		});
 		input.Relay.on('update', (value) => setRelay(value));
 	}
 
