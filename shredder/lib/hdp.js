5fe0e90b2d4e6d81cafea70ff312582136fcc86e
Juergen Fitschen <me@jue.yt>
2021-07-02 16:57:19 +0200

    shredder: update HDP

--- shredder/lib/hdp.js
+++ shredder/lib/hdp.js
@@ -3,6 +3,9 @@ const hdpFactory = require('homie-dgram');
 // Sigleton for HDPClient
 let hdpClient;
 module.exports = () => {
-	if (!hdpClient) hdpClient = hdpFactory();
+	if (!hdpClient) {
+		hdpClient = hdpFactory();
+		hdpClient.then((client) => client.triggerDiscovery());
+	}
 	return hdpClient;
 };
