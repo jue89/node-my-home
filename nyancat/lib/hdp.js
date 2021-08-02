d59a47ee468efeb344a0d2fbbd2deb58ef280b8c
Jue <me@jue.yt>
2021-08-02 22:17:58 +0200

    nyancat: update homie-dgram

--- nyancat/lib/hdp.js
+++ nyancat/lib/hdp.js
@@ -3,6 +3,9 @@ const hdpFactory = require('homie-dgram');
 // Sigleton for HDPClient
 let hdpClient;
 module.exports = () => {
-	if (!hdpClient) hdpClient = hdpFactory();
+	if (!hdpClient) {
+		hdpClient = hdpFactory({iface: 'homie0'});
+		hdpClient.then((client) => client.triggerDiscovery());
+	}
 	return hdpClient;
 };
