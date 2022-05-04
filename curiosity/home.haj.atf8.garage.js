696d87681a39608da8ab260e1cddc028ce902b09
Jue <me@jue.yt>
2022-05-04 19:28:30 +0200

    curiosity: add charging Homekit switch

--- curiosity/home.haj.atf8.garage.js
+++ curiosity/home.haj.atf8.garage.js
@@ -8,6 +8,15 @@ module.exports = [
 		output: {'On': `${BASE}.ceilingLight.desiredOnState`},
 		displayName: 'Ceiling Light'
 	}],
+
+	// Zero charging Homekit switch
+	[require('ftrm-homekit')('Switch'), {
+		name: 'garage-zero',
+		input: {'On': `${BASE}.zero.actualOnState`},
+		output: {'On': `${BASE}.zero.desiredOnState`},
+		displayName: 'Zero Charging'
+	}],
+
 	[require('ftrm-homekit')('TemperatureSensor'), {
 		name: 'garage-temp',
 		input: [{name: 'CurrentTemperature', pipe: `${BASE}.room.actualTemperature_degC`}],
