e06de7f70d7c8a93e9e356f6689c76c5220f4871
Jue <me@jue.yt>
2023-12-09 14:20:16 +0100

    powerranger: migrate to smart meter

--- powerranger/home.haj.atf8.sj.electricitymeter.js
+++ powerranger/home.haj.atf8.sj.electricitymeter.js
@@ -1,57 +1,81 @@
-const EventEmitter = require('events')
-const Poll = require('../_lib/poll.js');
-const Median = require('../_lib/median.js');
+const {SerialPort} = require('serialport');
+const {Writable} = require('stream');
+const {EventEmitter} = require('events');
+const {SmlFile} = require('open-sml');
 
-const WHPERPULSE = 1000 / 375;
-
-// Event emitter for pluses
-const electricityMeter = new EventEmitter();
-
-// Detect pulsed
-const p = new Poll('/sys/bus/iio/devices/iio:device0/in_voltage0_raw', 20);
-p.once('value', (initValue) => {
-	const denoise = new Median(5, initValue);
-	const baseLine = new Median(601, initValue);
-	
-	let lastRed = false;
-	let red = false;
-
-	p.on('value', (value) => {
-		value = denoise.step(value);
-		const base = baseLine.step(value);
-		const diff = value - base;
-
-		if (diff > 80) red = true;
-		if (diff < 50) red = false;
-
-		if (!lastRed && red) electricityMeter.emit('consumed', WHPERPULSE);
-
-		lastRed = red;
-	});
-});
-
-// Calc power
-let lastPulse;
-electricityMeter.on('consumed', (energy) => {
-	const now = Date.now();
-	if (lastPulse) {
-		const diff = now - lastPulse;
-		const pwr = 3600000 * energy / diff;
-		electricityMeter.emit('power', pwr);
+class DelayedTimeout {
+	constructor (fn, delay) {
+		this.fn = fn;
+		this.delay = delay;
 	}
-	lastPulse = now;
-});
 
-// Calc total energy
-const localStorage = require('../_lib/localStorage.js');
-if (!localStorage.totalEnergy) localStorage.totalEnergy = 0;
-electricityMeter.on('consumed', (energy) => {
-	localStorage.totalEnergy += energy;
-	electricityMeter.emit('totalEnergy', localStorage.totalEnergy);
-});
+	trigger () {
+		clearTimeout(this.to);
+		this.to = setTimeout(this.fn, this.delay);
+	}
+}
+
+class Cutter extends Writable {
+	constructor () {
+		super();
+		this.data = [];
+		this.to = new DelayedTimeout(() => {
+			this.emit('data', Buffer.concat(this.data));
+			this.data = [];
+		}, 500);
+	}
+
+	_write(chunk, enc, done) {
+		this.data.push(chunk);
+		this.to.trigger();
+		done();
+	}
+}
+
+class SmartMeter extends EventEmitter {
+	constructor (tty, offset = 0, step = 1) {
+		super();
+
+		let lastEvent = 0;
+		let pwr = [];
+
+		const sml = new Cutter();
+		tty.pipe(sml).on('data', (msg) => {
+			try {
+				const file = new SmlFile();
+				file.parse(msg);
+				const {messageBody} = file.messages.find(({messageTag}) => messageTag === 1793);
+				const {listEntries} = messageBody.valList;
+				const items = Object.fromEntries(listEntries.filter(({unit}) => unit > 0).map(({objName, unit, scaler, value}) => [objName.toString('hex'), {
+					unit: unit,
+					value: Math.pow(10, scaler) * value
+				}]));
+
+				const energyWh = items['0100010800ff'].value + offset;
+				const powerW = items['0100240700ff'].value;
+
+				pwr.push(powerW);
+
+				if (lastEvent + step < energyWh) {
+					lastEvent = energyWh;
+					const power = pwr.reduce((acc, val, arr) => acc + val, 0) / pwr.length;
+					pwr = [];
+					this.emit('power', power);
+					this.emit('totalEnergy', energyWh);
+				}
+			} catch (err) {
+				this.emit('error', err);
+			}
+		});
+	}
+}
+
+const tty = new SerialPort({path: '/dev/ttyUSB0', baudRate: 9600, parity: 'n', dataBits: 8, stopBits: 1});
+const electricityMeter = new SmartMeter(tty, 42238374);
 
 // Log into syslog
 electricityMeter.on('power', (pwr) => console.log(`Power consumption ${pwr}W`));
+electricityMeter.on('error', (err) => console.error(err.message));
 
 // Expose consumption onto the bus
 module.exports = [
@@ -65,3 +89,4 @@ module.exports = [
 		bus: electricityMeter
 	}]
 ];
+
